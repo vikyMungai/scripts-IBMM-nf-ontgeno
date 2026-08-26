1a959cdf3ee939dcc5a779c4ce58033a6de0c7cf
vmungai <vittoria.mungai@unibe.ch>
2026-08-26 16:56:46 +0200

    add micromamba env activation inside script and output messages

--- reference_genome/create_fai_dict_from_fasta.sh
+++ reference_genome/create_fai_dict_from_fasta.sh
@@ -10,11 +10,30 @@
 
 # Example to execute the script (without genotype_model) from repo directory
 # ./reference_genome/create_fai_dict_from_fasta.sh "/home/user_ubuntu/data/references/reference_adaptive_sampling/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz"
+
+CURR_DIR=$(pwd)
+# activate micromamba nf-core environment to have samtools 
+cd 
+cd programs/micromamba/
+source bin/init.sh 
+micromamba activate nf-core
+cd 
+cd ${CURR_DIR}
+
 REF_FNA_ZIP="$1"
 REF_FNA="${REF_FNA_ZIP%.*}"
 DICT_FILE="${REF_FNA%.*}.dict"
 
 
-gzip -dk "${REF_FNA_ZIP}"
-samtools faidx "${REF_FNA}"
-samtools dict "${REF_FNA}" --output ${DICT_FILE}
+#gzip -dk "${REF_FNA_ZIP}"
+echo "the reference file was unzipped"
+
+#samtools faidx "${REF_FNA}"
+echo "the index of the reference file was created"
+
+#samtools dict "${REF_FNA}" --output ${DICT_FILE}
+echo "the reference file's dictionary was created"
+
+# deactivate the environment 
+micromamba deactivate
+
